a9db257d675532e4d71139dd1ac8c2c0337231e4
htoo123456 <90297565+htoo123456@users.noreply.github.com>
2021-10-19 08:49:25 +0630

    Update script.js

--- script.js
+++ script.js
@@ -4,43 +4,51 @@ const calculate = (num) {
   return new Function("return " + num)();
 }
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", (e) => {
-    if(input.textContent == "0" && e.target.value != "Del" && e.target.value != "=") {
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    if(e.target.classList.contains("num")) {
+      e.target.style.backgroundColor = "#fff";
+      setTimeout(() => {
+        e.target.style.backgroundColor = "hsl(30, 25%, 89%)"
+      }, 100);
+    } else {
+      e.target.style.backgroundColor = "#ede3c2";
+      setTimeout(() => {
+        e.target.style.backgroundColor = "#ffc106"
+      }, 200);
+    }
+    if (input.textContent == "0" && e.target.value != "Del" && e.target.value != "=") {
       input.textContent = "";
     }
 
     switch (e.target.value) {
       case "=":
-        const changingValue = (input1) => {
-          const acmultiple = input1.replaceAll("×", "*");
-          const acdivide = acmultiple.replaceAll("÷", "/");
+        let changingValue = (input1) => {
+          let acmultiple = input1.replaceAll("×", "*");
+          let acdivide = acmultiple.replaceAll("÷", "/");
           return acdivide;
         };
-        const value = /×|÷|^|√/.test(input.textContent)
+        let value = /×|÷|^|√/.test(input.textContent)
           ? changingValue(input.textContent)
           : input.textContent;
         try {
-         input.textContent = calculate(value);
+          input.textContent = calculate(value);
         } catch (error) {
           console.log(error);
         }
-        
+
         break;
       case "Del":
-        if(input.textContent == "0") {
-          return;
-        } else {
           const cleanedValue = input.textContent.substring(
-          0,
-          input.textContent.length - 1
-        );
+            0,
+            input.textContent.length - 1
+          );
           if(cleanedValue == "") {
             input.textContent = "0";
           } else {
             input.textContent = cleanedValue;
           }
-        }
+        
         break;
       case "AC":
         input.textContent = "0";
@@ -50,4 +58,5 @@ for (let i = 0; i < buttons.length; i++) {
         break;
     }
   });
-}
+})
+
